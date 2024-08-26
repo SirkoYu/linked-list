8b45dbce273d438008833cc6347e49240aad62a1
Yurii Sirko <urasir1999@gmail.com>
2024-08-26 19:26:00 +0300

    refactor documentation

--- src/LinkedList.java
+++ src/LinkedList.java
@@ -1,24 +1,32 @@
 /**
- * <p>An object for storing a single node of linked list. Has five attributes:</p>
- * <ul>
- * <li>data - stores an integer node value.</li>
- * <li>nextNode - link to next node in linked list.</li>
- * <li>previousNode - link to previous node in linked list.</li>
- * <li>isHead - boolean value (is true when node is head of the list)</li>
- * <li>isTail - boolean value (is true when node is tail of the list)</li>
- * </ul>
+ * <p>Singly linked list.</p>
  * @author ITryHard
  */
-
 public class LinkedList {
-    private Node head;
-    private Node tail;
+    private Node head = null;
+    private Node tail = null;
 
-    private int size;
+    private int size = 0;
 
     public int size(){
         return this.size;
     }
+    public boolean isEmpty(){
+        return size==0;
+    }
+
+
+    /**
+     * <p>An object for storing a single node of linked list. Has five attributes:</p>
+     * <ul>
+     * <li>data - stores an integer node value.</li>
+     * <li>nextNode - link to next node in linked list.</li>
+     * <li>previousNode - link to previous node in linked list.</li>
+     * <li>isHead - boolean value (is true when node is head of the list).</li>
+     * <li>isTail - boolean value (is true when node is tail of the list).</li>
+     * </ul>
+     * @author ITryHard
+     */
     static class Node {
         private int data;
         private Node nextNode = null;
